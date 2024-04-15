61602dc1de54621260582c4954dd03f8fde83ff8
Nikhil <nk9599094257@gmail.com>
2024-04-15 23:33:21 +0530

    w

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express  = requier("express");
 const app = express();
+const Router = express.Router()
+
+Router.get("/" , (req , res)=>{
+    res.status(200).json({msg : "this is setting"})
+})
+
 
 app.listen(3000, ()=>{
     console.log("stareted");
