e5b845a2adcdd386826dcce917232e958d81c408
Nikhil <nk9599094257@gmail.com>
2024-04-16 15:44:23 +0530

    check this

--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const Router = express.Router();
 
-Router.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).json({ msg: "this is setting hello " });
 });
 
-app.use("/", Router);
+// app.use("/", Router);
 
 app.listen(3000, () => {
     console.log("started");
 });
+
+module.exports = app
